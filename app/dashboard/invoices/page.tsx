8e12b88715799f49963f0aa52cc027e29a10c4cb
German Levental <gerleven@gmail.com>
2024-02-13 22:25:10 -0300

    some comments and clean code

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,19 +6,18 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
-export default async function Page({
-    searchParams,
-  }: {
-    searchParams?: {
-      query?: string;
-      page?: string;
-    };
-  }) {
-    const query = searchParams?.query || "";
-    const currentPage = Number(searchParams?.page) || 1;
-    
-    const totalPages  = await fetchInvoicesPages(query);
+
+interface invoicesPageParams {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}
+
+export default async function Page({ searchParams }: invoicesPageParams) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -29,7 +28,8 @@ export default async function Page({
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        {/*Normally, the Suspense appear only the first time, but adding and updating the key we are force to re-appear again in each search update */}
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -4,27 +4,32 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { useDebouncedCallback } from 'use-debounce';
 
-
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
-  const router = useRouter();
-  const {replace} = useRouter(); //podriamos destructurar el router.replace para usar directamente "replace" en lugar de "router.replace"
+  const { replace } = useRouter(); //useRouter has many methods, we'll use only replace.
   const pathname = usePathname();
 
-  const handleSearch = useDebouncedCallback((term: string)=>{
+  const searchRedirect = (term: string) => {
     console.log(`searching: ${term}`);
-    const params = new URLSearchParams(searchParams);
-    params.set('page','1');
-    if(term){
+    // const params = new URLSearchParams(window.location.search);
+    
+    //Get params:
+    const params = new URLSearchParams(searchParams); //we don't need any more the "window.location.search" to pass in URLSearchParams(window.location.search); because now we have the useSearchParams() hook
+    
+    //Set new params (page and query):
+    params.set('page', '1');
+    if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+
     // const newPath = `${pathname}?${params.toString()}`;
-    // console.log("Navegando a ",newPath);
-    router.replace(`${pathname}?${params.toString()}`);
-  },300);
-  
+    // console.log("Navigating to: ",newPath);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  const handleSearch = useDebouncedCallback(searchRedirect, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -34,8 +39,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e)=>{handleSearch(e.target.value)}}
-        defaultValue={searchParams.get('query')?.toString()} //Now if you refresh the page the input will fill the input acording the url param
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get('query')?.toString()} //Now if you refresh the page the input will keep on sync with the url param
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
